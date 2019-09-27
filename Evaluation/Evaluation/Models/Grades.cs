83ac3052e1a48ab3ed73126a52b17427954d0823
Gustavo Camacho Pereyra <gustavocamachopereyra@gmail.com>
2019-09-27 15:15:15 -0400

    modelosok

---
+++ Evaluation/Evaluation/Models/Grades.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Evaluation.Models
+{
+    public class Grades
+    {
+        [Key]
+        public int NID { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public int Grade { get; set; }
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public Students Student { get; set; }
+    }
+
+}
---
+++ Evaluation/Evaluation/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Evaluation.Models
+{
+    public class Students
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Birthday { get; set; }
+        [Required]
+        public virtual ICollection<Grades> Grades { get; set; }
+    }
+
+}
